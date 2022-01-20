3f9825919e114511bd88ac38856a3868b2aa2397
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-01-20 15:45:53 +0530

    Add files via upload

---
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int>res;
+        int n=nums.size();
+        for(int i=0;i<n;i+=2){
+            res.insert(res.end(),nums[i],nums[i+1]);
+        }
+        return res;
+    }
+};
