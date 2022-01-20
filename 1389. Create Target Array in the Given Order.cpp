922afc080534346216801e655388aa83c0256c5b
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-01-20 15:46:14 +0530

    Add files via upload

---
+++ 1389. Create Target Array in the Given Order.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        int n=nums.size();
+        vector<int>res;
+        for(int i=0;i<n;i++){
+            res.insert(res.begin()+index[i],nums[i]);
+        }
+        return res;
+    }
+};
