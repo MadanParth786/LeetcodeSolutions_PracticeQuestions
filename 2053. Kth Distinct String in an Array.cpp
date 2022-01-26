7d72d386bc2d3ba38f4eb2fc32038af557bd3373
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-01-26 22:08:41 +0530

    Add files via upload

---
+++ 2053. Kth Distinct String in an Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        int n=arr.size();
+        vector<string>res;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(arr[i]!=arr[j]){
+                    res.push_back(arr[i]);
+                    //res.push_back(arr[n-1]);
+                }
+                else{
+                    res.push_back("");
+                }
+            }
+        }
+        return(res[k]);
+    }
+};
