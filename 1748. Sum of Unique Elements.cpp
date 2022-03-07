2d46755ddf9e3a8d52abc060059c92b32770a473
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-03-07 14:09:25 +0530

    Add files via upload

---
+++ 1748. Sum of Unique Elements.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int cnt[101] = {}, res = 0;
+    for (auto n : nums)
+        ++cnt[n];
+    for (auto i = 1; i <= 100; ++i)
+        if (cnt[i] == 1)
+            res += i;
+    return res;
+    }
+};
