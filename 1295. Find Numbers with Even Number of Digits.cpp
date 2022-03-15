5356d23003b42b2d58c3a00215adcfb7639e0e14
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-03-15 12:00:59 +0530

    Add files via upload

---
+++ 1295. Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if((9<=nums[i] and nums[i]<=99) or (1000<=nums[i] and nums[i]<=9999) or (nums[i]==10000)){
+                count+=1;
+            }
+        }
+        return count;
+    }
+};
