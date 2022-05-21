32a34b0ce679cecfbdba8cb6510b8e45dd1f810a
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-05-21 09:31:47 +0530

    Update 16. 3Sum Closest.cpp

--- 16. 3Sum Closest.cpp
+++ 16. 3Sum Closest.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
+    int ThreeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
         int res = nums[0] + nums[1] + nums[2];
         for(int i = 0; i < nums.size() - 2; i++){
