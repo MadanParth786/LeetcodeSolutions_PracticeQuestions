422b511731fccec14bd72cffc5c057c5f2384b1d
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-05-27 10:23:16 +0530

    Update 12. Integer to Roman.cpp

--- 12. Integer to Roman.cpp
+++ 12. Integer to Roman.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    const int valu[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    const int value[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     const string rom[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
     string intToRoman(int num) {
         string ans = "";
