fd0b079285790203ae08f4e27dbfd1d58568797c
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-05-29 09:03:35 +0530

    Update 1313. Decompress Run-Length Encoded List.cpp

--- 1313. Decompress Run-Length Encoded List.cpp
+++ 1313. Decompress Run-Length Encoded List.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> decompressRLElist(vector<int>& nums) {
+    vector<int> DecompressRLElist(vector<int>& nums) {
         vector<int>res;
         int n=nums.size();
         for(int i=0;i<n;i+=2){
