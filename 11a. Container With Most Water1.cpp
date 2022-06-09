5898e20f071409955e6df68801ecc90253d02dc2
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-06-09 11:40:32 +0530

    Update 11a. Container With Most Water1.cpp

--- 11a. Container With Most Water1.cpp
+++ 11a. Container With Most Water1.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
+    int MaxArea(vector<int>& height) {
         int water = 0;
     int i = 0, j = height.size() - 1;
     while (i < j) {
