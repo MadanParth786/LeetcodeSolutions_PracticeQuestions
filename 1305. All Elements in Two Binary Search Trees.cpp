31bac728922ff0707bbd2f8e0bda44fa79cdbca0
Parth Madan <69120382+MadanParth786@users.noreply.github.com>
2022-06-14 08:06:23 +0530

    Update 1305. All Elements in Two Binary Search Trees.cpp

--- 1305. All Elements in Two Binary Search Trees.cpp
+++ 1305. All Elements in Two Binary Search Trees.cpp
@@ -21,7 +21,7 @@ public:
           dfs(root->left);
           dfs(root->right);
      }
-     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+     vector<int> getAllElements(treeNode *root1, TreeNode *root2)
      {
           dfs(root1);
           dfs(root2);
